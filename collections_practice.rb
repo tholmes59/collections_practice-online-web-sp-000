366a1d0c260b876a81d960e11e3197fc40eb5965
Tom Holmes <tholmes59@gmail.com>
2019-01-17 23:52:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+    new_array = []
+    array.each do |word|
+    new_array << word.insert(2, "$")
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select {|letter| letter.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, x| sum + x} 
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word|
+    "#{word}s"
+  end
+end
