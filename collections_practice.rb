4fb801b299a0e0873d31c10f2dbe5f8875b354b3
Tom Holmes <tholmes59@gmail.com>
2019-01-18 00:25:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  def swap_elements_from_to(x, y)
+  def self(x, y)
       self[x], self[y] = self[y], self[x]
   end
   array.self!(1,2) 
