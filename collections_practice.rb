95b12679f734b1c235087a8f35bfc170de8109c1
Tom Holmes <tholmes59@gmail.com>
2019-01-18 00:07:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |word|
-    "#{word}s"
+  array.each_with_index.collect do |word, index|
+    if index == 1 
+      word
+      else
+      "#{word}s"
+    end
   end
 end
