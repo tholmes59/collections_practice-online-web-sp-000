2539dc3010a23b91ee9015b1be456e8e172cc240
Tom Holmes <tholmes59@gmail.com>
2019-01-18 00:29:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  def swap_elements_from_to(array, index, destination_index)
+  def swap_elements_from_to(index, destination_index)
       self[index], self[destination_index] = self[destination_index], self[index]
   end
   array.swap_elements_from_to(1,2) 
