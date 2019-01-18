8e42e26acf76e376219b462a530f00242f0d5b77
Tom Holmes <tholmes59@gmail.com>
2019-01-18 00:04:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,11 +18,10 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-    new_array = []
     array.each do |word|
-    new_array << word[2] = "$"
+    word[2] = "$"
+    word
   end
-  new_array
 end
 
 def find_a(array)
